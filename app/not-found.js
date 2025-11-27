197de54b91092f0b2b1e211ff5f9f6a01e579678
sobahus <sobahusn27@gmail.com>
2025-11-27 19:38:37 +0700

    Add: New file and Setup

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center text-center space-y-4">
+      <p className="font-bold text-4xl">Not Found</p>
+      <p className="font-semibold text-xl">Halamannya gada COYY!!!</p>
+      <Link href="/" >
+        <button className='bg-sky-200 text-gray-600 px-4 py-2 rounded-lg'>Return HOME?</button> 
+      </Link>
+    </div>
+  )
+}
