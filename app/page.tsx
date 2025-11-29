2ffbc8c68f72c3c82f04fbb4dcc00f9d81105507
sobahus <sobahusn27@gmail.com>
2025-11-29 23:36:15 +0700

    refactor: remove ScrollVelocity component and its styles from the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Nav from "./components/Navbar";
-import Image from "next/image";
-import ScrollVelocity from "./component/ScrollVelocity";
 
 export default function Page() {
   return (
