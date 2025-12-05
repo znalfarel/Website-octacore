8036fdf4fcd3cc9aa2c53e75cce41f49c3472ed7
znalfarel <harvestscary@gmail.com>
2025-12-05 21:43:37 +0700

    fixxing bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Nav from "./components/Navbar";
 import Testimoni from "./components/Testimoni";
-import Portfolio from "./components/Portfolio";
+// import Portfolio from "./components/Portfolio";
 
 export default function Page() {
   return (
     <>
       <Nav />
       <HeroSection />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Testimoni />
       <Footer />
     </>
