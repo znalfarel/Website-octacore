48b39450a51af8f79837e052a35a3c468e9615d6
znalfarel <harvestscary@gmail.com>
2025-12-13 06:34:57 +0700

    style: add waves

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -172,6 +172,22 @@ export default function HeroSection() {
             </div>
           </div>
         </div>
+
+        <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] z-20">
+            <svg 
+                className="relative block w-[calc(100%+1.3px)] h-[50px] sm:h-[100px] md:h-[120px]" 
+                data-name="Layer 1" 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 1200 120" 
+                preserveAspectRatio="none"
+            >
+                <path 
+                    d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+                    fill="#0f172a"
+                    transform="scale(1, -1) translate(0, -120)" 
+                ></path>
+            </svg>
+        </div>
       </section>
 
       {/* COMPANY PROFILE SECTION */}
